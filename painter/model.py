1a6af48093e63a72af8ff8b53874df6bf6c68574
Carimannola <jruizt034@gmail.com>
2024-08-06 13:10:02 -0500

    create Painter class

--- painter/model.py
+++ painter/model.py
@@ -1,14 +1,15 @@
 # TODO: Add code here
 import math
 import matplotlib.pyplot as plt
+import pickle
 
 class Point:
-    def __init__(self, x:float, y:float):
+    def __init__(self, x:float, y:float) -> None:
         self.x=x
         self.y=y
 
 class Circle:
-    def __init__(self, center:Point, radius:float):
+    def __init__(self, center:Point, radius:float) -> None:
         self.center=center
         self.radius=radius
     def area(self) -> float:
@@ -23,7 +24,7 @@ class Circle:
         return f"Circle with center at ({self.center.x},{self.center.y}) and radius {self.r}"
 
 class Triangle:
-    def __init__(self, point_1:Point, point_2:Point, point_3:Point):
+    def __init__(self, point_1:Point, point_2:Point, point_3:Point) -> None:
         self.point_1=point_1
         self.point_2=point_2
         self.point_3=point_3
@@ -43,7 +44,7 @@ class Triangle:
         return f"Triangle with vertices at ({self.point_1.x},{self.point_1.y}),({self.point_2.x},{self.point_2.y}) and ({self.point_3.x},{self.point_3.y})"
 
 class Rectangle:
-    def __init__(self, point_1:Point, point_2:Point):
+    def __init__(self, point_1:Point, point_2:Point) -> None:
         self.point_1=point_1
         self.point_2=point_2
     
@@ -63,3 +64,32 @@ class Rectangle:
     def __str__(self) -> str:
         return f"Rectangle with vertices at ({self.point_1.x},{self.point_1.y}) and ({self.point_2.x},{self.point_2.y})"
     
+class Painter:
+    
+    FILE = ".painter"
+    
+    def __init__(self) -> None:
+        self.shapes: list = []
+        self._load()
+
+    def _load(self) -> None:
+        try:
+            with open(Painter.FILE, "rb") as f:
+                self.shapes = pickle.load(f)
+        except (EOFError, FileNotFoundError):
+            self.shapes = []
+    
+    def _save(self) -> None:
+        with open(Painter.FILE, "wb") as f:
+            pickle.dump(self.shapes, f)
+    
+    def add_shape(self, shape) -> None:
+        self.shapes.append(shape)
+        self._save()
+
+    def total_area(self) -> float:
+        return sum(shape.area() for shape in self.shapes)
+    
+    def clear(self) -> None:
+        self.shapes = []
+        self._save()
