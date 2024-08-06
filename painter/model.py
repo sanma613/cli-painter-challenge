8b128ddec2436831172a3e6fa9fb5def4b7c1d68
Carimannola <jruizt034@gmail.com>
2024-08-06 12:22:00 -0500

    Create Circle class

--- painter/model.py
+++ painter/model.py
@@ -4,3 +4,8 @@ class Point:
     def __init__(self, x:float, y:float):
         self.x=x
         self.y=y
+
+class Circle:
+    def __init__(self, center:Point, radius:float):
+        self.center=center
+        self.radius=radius
