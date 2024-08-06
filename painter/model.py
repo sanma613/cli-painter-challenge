d6b263f6e210312550bf5da21839bec5a0bfda0d
Carimannola <jruizt034@gmail.com>
2024-08-06 12:38:03 -0500

    create method draw

--- painter/model.py
+++ painter/model.py
@@ -1,5 +1,6 @@
 # TODO: Add code here
 import math
+import matplotlib as plt
 
 class Point:
     def __init__(self, x:float, y:float):
@@ -12,4 +13,11 @@ class Circle:
         self.radius=radius
     def area(self) -> float:
         area= math.pi * (self.radius**2)
+        return area
+    def draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+
     
